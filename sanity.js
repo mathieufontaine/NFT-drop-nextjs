67c8aabdd2c4466da7c58c53b7173906f43d137f
mathieufontaine <mathieufontaine.dev@gmail.com>
2022-10-06 16:02:29 +0200

    fixed typo config

--- sanity.js
+++ sanity.js
@@ -2,12 +2,12 @@ import { createClient, createCurrentUserHook } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const config = {
-  projectId: process.env.NEXT_PULBIC_SANITY_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PULBIC_SANITY_DATASET || "production",
+  token: process.env.SANITY_API_TOKEN,
   apiVersion: "2021-03-25",
-  useCdn: process.env.NODE_ENV === "production",
+  useCDN: process.env.NODE_ENV === "production",
 };
 
 export const sanityClient = createClient(config);
-
 export const urlFor = (source) => imageUrlBuilder(config).image(source);
